af2adfabe6649fa54508e5ad20792e6e7107cbbc
veronika-simic <veronikasimic21@gmail.com>
2023-06-29 09:07:26 +0200

    place cards in a grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import Typography from '@mui/material/Typography';
+import { Button, Grid } from '@mui/material';
 import BasicCard from './components/BasicCard';
 
 function App() {
-  return <BasicCard />;
+  return (
+    <>
+      <Typography>
+        Learn electronics and coding <br /> with our best-selling DIY STEM kits:
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item>
+          <BasicCard />
+        </Grid>
+        <Grid item>
+          <BasicCard />
+        </Grid>
+        <Grid item>
+          <BasicCard />
+        </Grid>
+      </Grid>
+      <Button variant="contained">Show more</Button>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Container from '@mui/material/Container';
 import App from './App';
 import './index.css';
 
