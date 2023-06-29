05fc8632fbc6be1cba024d289274d2e99682b268
veronika-simic <veronikasimic21@gmail.com>
2023-06-29 14:18:15 +0200

    change image alt

--- src/components/BasicCard.tsx
+++ src/components/BasicCard.tsx
@@ -24,7 +24,7 @@ export default function BasicCard(props: ProductData) {
         <CardMedia
           component="img"
           image={data.imgSrc}
-          alt={data.text}
+          alt={data.title}
           style={{ height: '16rem' }}
         />
 
